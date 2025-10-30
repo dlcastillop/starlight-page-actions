82c99d387d2e80dece190c886a8dd118f42a7adc
Daniel Castillo <dlcastillo3015@gmail.com>
2025-10-30 10:34:16 -0400

    feat(docs): update starlight documentation

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -3,27 +3,44 @@ import { defineConfig } from "astro/config";
 import starlightPageActions from "starlight-page-actions";
 
 export default defineConfig({
+  site: "https://starlight-page-actions.dlcastillop.com",
   integrations: [
     starlight({
-      editLink: {
-        baseUrl:
-          "https://github.com/dlcastillop/starlight-page-actions/edit/main/docs/",
-      },
-      plugins: [starlightPageActions()],
+      title: "Starlight Page Actions",
       sidebar: [
         {
-          label: "Start Here",
-          items: ["getting-started"],
+          label: "Overview",
+          items: [
+            {
+              label: "Getting Started",
+              link: "docs/getting-started",
+            },
+          ],
         },
-      ],
-      social: [
         {
-          href: "https://github.com/dlcastillop/starlight-page-actions",
-          icon: "github",
-          label: "GitHub",
+          label: "Guides",
+          items: [
+            {
+              label: "Personalize the prompt",
+              link: "docs/getting-started",
+            },
+            {
+              label: "Generate the llms.txt file",
+              link: "docs/getting-started",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          items: [
+            {
+              label: "Configuration Reference",
+              link: "docs/getting-started",
+            },
+          ],
         },
       ],
-      title: "starlight-page-actions",
+      plugins: [starlightPageActions()],
     }),
   ],
 });
